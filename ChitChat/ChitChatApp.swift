612e2a289a3ee2040e720a13d7626e7cde1a3f12
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-06-12 15:50:00 +0530

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Nimrat Khalsa on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
